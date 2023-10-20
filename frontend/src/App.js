98f959bc74f00edf68fc72e623e1e976c81c02ce
Si manglam <90334583+simanglam@users.noreply.github.com>
2023-10-21 01:13:49 +0800

    React Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
 function App() {
+  const BASEURL = "https://4b0d-2001-b400-e606-494e-3440-e668-928d-200d.ngrok-free.app"
+  const handleClick = () => {
+    axios.post(BASEURL).then().then()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+      <div className='transformed'>
+        <Button variant = "danger" onClick={handleClick}>擲骰</Button>
+      </div>
     </div>
   );
 }
